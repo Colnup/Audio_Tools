426af08f323d778626f94158e823d24138517ada
Picolin <picolin@picolin.com>
2020-08-25 16:52:19 +0200

    Auto installer

---
+++ bin/Package/atinstall.bat
@@ -0,0 +1,7 @@
+@echo off
+
+pip3 install --upgrade youtube-dl
+
+git clone https://github.com/Colnup/Audio_Tools
+
+start ./Audio_Tools/interfact.pyw
---
+++ bin/Package/pyinstall.bat
@@ -0,0 +1,3 @@
+@echo off
+
+python-3.8.5.exe /quiet InstallAllUsers=1 PrependPath=1
